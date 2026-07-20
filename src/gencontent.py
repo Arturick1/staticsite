54294a6c31582a9028fae42deb2e05df86494b12
Arturick <wdrellpsc@gmail.com>
2026-07-20 02:25:37 -0500

    Moved destination from public to docs

--- src/gencontent.py
+++ src/gencontent.py
@@ -1,7 +1,7 @@
 import os
 from markdown_blocks import markdown_to_html_node, extract_title
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating a page from {from_path} to {dest_path} using {template_path}.")
     with open(from_path, encoding="utf-8") as f:
         md = f.read()
@@ -12,13 +12,15 @@ def generate_page(from_path, template_path, dest_path):
     title = extract_title(md)
     template = template.replace("{{ Title }}", title)
     template = template.replace("{{ Content }}", new_html)
+    template = template.replace('href="/', 'href="{basepath}')
+    template = template.replace('src="/', 'src="{basepath}')
     dest = os.path.dirname(dest_path)
     if not os.path.exists(dest):
         os.makedirs(dest)
     with open(dest_path, 'w', encoding="utf-8") as f:
         f.write(template)
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     if not os.path.exists(dest_dir_path):
         os.mkdir(dest_dir_path)
     content = os.listdir(dir_path_content)
@@ -27,8 +29,8 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
         dest_path = os.path.join(dest_dir_path, entry)
         if os.path.isfile(from_path):
             dest_path = dest_path.replace(".md", ".html")
-            generate_page(from_path, template_path, dest_path)
+            generate_page(from_path, template_path, dest_path, basepath)
         else:
-            generate_pages_recursive(from_path, template_path, dest_path)
+            generate_pages_recursive(from_path, template_path, dest_path, basepath)
 
 
--- src/main.py
+++ src/main.py
@@ -1,24 +1,32 @@
 import os
 import shutil
+import sys
 from copystatic import copy_files_recursive
 from gencontent import generate_pages_recursive
 
 dir_path_static = "./static"
-dir_path_public = "./public"
+dir_path_docs = "./docs"
 dir_path_content = "./content"
 template_path = "./template.html"
 
 def main():
 
-    if os.path.exists(dir_path_public):
-        shutil.rmtree(dir_path_public)
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
 
-    copy_files_recursive(dir_path_static, dir_path_public)
+    else:
+        basepath = "/"
+
+    if os.path.exists(dir_path_docs):
+        shutil.rmtree(dir_path_docs)
+
+    copy_files_recursive(dir_path_static, dir_path_docs)
 
     generate_pages_recursive(
         dir_path_content,
         template_path,
-        dir_path_public,
+        dir_path_docs,
+        basepath,
     )
 
 main()
